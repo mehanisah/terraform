2694077496a443f86ebbe20262aa33e7f57c58f8
mehanisah <156770138+mehanisah@users.noreply.github.com>
2025-02-02 09:22:37 +0800

    Update main.tf
    add ,

--- modules/vm_instance/main.tf
+++ modules/vm_instance/main.tf
@@ -75,7 +75,7 @@ provisioner "remote-exec" {
         "echo \"deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
         "sudo apt update",
         "sudo apt install -y docker-ce docker-ce-cli containerd.io",
-        "sudo usermod -aG docker ansible"
+        "sudo usermod -aG docker ansible",
         "sudo apt install -y nfs-common"
     ]
 }
